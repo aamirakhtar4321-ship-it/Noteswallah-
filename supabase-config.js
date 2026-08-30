213a1385c1a0722ca346e2458b8d4e7addbc6011
aamirakhtar4321-ship-it <aamirakhtar4321@gmail.com>
2026-08-30 11:23:26 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://zctzpiwjkzmxteatwggr.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_yIxRd6uRpXS4WOeRE8fvQA_F75h8sxP";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
